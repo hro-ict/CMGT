ea16deffa5040978e04ea31d1a05aefca357785c
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-09-18 10:43:52 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\News;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get("/",[News::class, 'index']);
 Route::get("/articles",[News::class, 'articles'])->name('articles');
 Route::post("/check_login",[News::class, 'login'])->name('check_login');
