f1c4e6a5ffd4d5741e5b0cbeec59bff2617caea3
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-10-05 21:44:58 +0200

    Update 2022_10_02_113603_articles.php

--- database/migrations/2022_10_02_113603_articles.php
+++ database/migrations/2022_10_02_113603_articles.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->text("title")->nullable();
+            $table->text("body")->nullable();
+            $table->text("foto")->nullable();
+            $table->integer("user_id")->nullable();
             $table->timestamps();
         });
     }
