a6fcddbf65dc110b72d21b811c75b8dd234f3a8c
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-10-09 18:40:06 +0200

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,20 +5,24 @@
        @if($errors->any())
        <h3 class="alert-danger text-center">Registration is not succesfull</h3>
        @endif
-            <form method="post" enctype="multipart/form-data"  action="{{route('create_user')}}">
+            <form method="post" enctype="multipart/form-data"  action="{{route('save_register')}}">
                 @csrf
                       <div class=" col-md-6 mb-3 mt-3">
                         <label for="firstname" class="form-label">Firstname:</label>
                         <input type="text" class="form-control" id="firstname" placeholder="Enter firstname" name="firstname">
                           @if ($errors->has('firstname'))
                           <span class="text-danger">{{ $errors->first('firstname') }}</span>
+                          @else
+                          <span class="form-text"><mark></mar>* Required</mark></span>
                           @endif
                         </div>
                         <div class=" col-md-6 mb-3 mt-3">
                           <label for="lastname" class="form-label">Lastname:</label>
                           <input type="text" class="form-control" id="lastname" placeholder="Enter lastname" name="lastname">
                             @if ($errors->has('firstname'))
-                            <span class="text-danger">{{ $errors->first('firstname') }}</span>
+                            <span class="text-danger">{{ $errors->first('lastname') }}</span>
+                            @else
+                          <span class="form-text"><mark></mar>* Required</mark></span>
                             @endif
                           </div>
                         <div class="col-md-6 mb-3 mt-3">
@@ -26,6 +30,8 @@
                         <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
                         @if ($errors->has('username'))
                         <span class="text-danger">{{ $errors->first('username') }}</span>
+                          @else
+                          <span class="form-text"><mark></mar>* Required. min 4 max 15 characters</mark></span>
                         @endif
                         </div>
                         <div class=" col-md-6 mb-3 mt-3">
@@ -33,6 +39,8 @@
                           <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                           @if ($errors->has('email'))
                           <span class="text-danger">{{ $errors->first('email') }}</span>
+                          @else
+                          <span class="form-text"><mark></mar>* Required</mark></span>
                           @endif
                         </div>
                         <div class="mb-3 col-md-6">
@@ -40,11 +48,13 @@
                           <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
                           @if ($errors->has('password'))
                           <span class="text-danger">{{ $errors->first('password') }}</span>
+                          @else
+                          <span class="form-text"><mark></mark>* Required. min 6 max 15 characters</mark></span>
                           @endif
                         </div>
                         <div class="mb-3 col-md-6">
-                          <label for="password_confirm" class="form-label">Confirm password:</label>
-                          <input type="password" class="form-control" id="password_confirm" placeholder="Confirm password" name="password_confirm">
+                          <label for="password_confirmation" class="form-label">Confirm password:</label>
+                          <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm password" name="password_confirmation">
                         </div>
                         {{-- <div class="form-check mb-3">
                           <label class="form-check-label">
@@ -60,3 +70,4 @@
                       </form>
           </div>
 @endsection
+          
