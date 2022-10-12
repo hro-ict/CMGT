1d3ea829a8667bea720f1cab8bfd9e2dadd36eab
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-10-12 22:18:24 +0200

    Update Users.php

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -1,21 +1,83 @@
 <?php
-namespace App\Listeners;
+
 namespace App\Http\Controllers;
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use App\Models\User;
-
+use App\Models\Users;
+use App\Models\Articles;
+use App\Models\Categories;
+use App\Models\Comments;
+use App\Models\Tokens;
+use Illuminate\Support\Facades\Hash;
+use Mail;
 
 class Users extends Controller
 {
-    public function create_user(Request $request){
+    
+       public function register() {
+        return view("register");
+    }
+    
+    
+    public function login(Request $request){
+          $request->validate([
+             "password"=>"required",
+             "username"=>"required",
+             ]);
+        $username= $request->username;
+        $password= $request->password;
+
+        if (Users::where('username', $username)->exists()) {
+            $data= Users::where("username", $username)->get()[0];
+            $user_id= $data->id;
+            $user= Users::find($user_id);
+            $role= $user->get_role->role;
+            $comment_count= $user->get_user_comments->count();
+         
+         
+            if (md5($password)==$data->password){
+                $session_data= array(
+                    "id"=>$user_id,
+                    "username"=>$username,
+                    "name"=>$data->firstname." ".$data->lastname,
+                    "foto"=>$data->foto,
+                    "role"=>$role,
+                    "comment_count"=>$comment_count
+                );
+                Session::put("session", $session_data);
+
+
+               return redirect()->to("/");
+        
+            }  
+            else{
+                return view("/login", ["response"=>"failed"]) ;
+             }  
+         }
+        else{
+            return view("/login", ["response"=>"failed"]);  
+        }
+       
+        // Session::forget("username");
+
+    }
+
+//controller__users
+    public function logout(){
+        Session::forget("session");
+        return redirect()->to('/');
+    }
+
+//controller__users
+    public function save_register(Request $request){
         $firstname= htmlspecialchars($request->firstname);
         $lastname= htmlspecialchars($request->lastname);
         $email= htmlspecialchars($request->email);
         $password=  htmlspecialchars(md5($request->password));
         $username= htmlspecialchars(strtolower($request->username));
         $role_id= 2;
-        $created_at= date("d-m-Y");
         if ($request->foto){
             $foto= $username.".png";
             $request->foto->move(public_path('images'), $foto);
@@ -31,57 +93,138 @@ class Users extends Controller
              "firstname"=>"required|max:50|min:2",
              "lastname"=>"required|max:50|min:2",
              "email"=>"required|email|unique:users",
-             "password"=>"required|min:6|max:10|confirmed",
-             "username"=>"required|unique:users|min:5|max:15",
-             "password"=>"required"
+             "password"=>"required|min:6|max:15|confirmed",
+             "username"=>"required|unique:users|min:4|max:15",
+         
 
              ]);
-        $data= compact("firstname","lastname","email","password","username","foto","role_id", "created_at");
+        $data= compact("firstname","lastname","email","password","username","foto","role_id");
              
-         User::create($data);
+         Users::create($data);
          return redirect()->to("/login");
         
-
     }
-
-    public function check_login(Request $request){
-        $username= $request->username;
-        $password= $request->password;
-
-  
-        
-
+    
+    
+    public function get_all_users(){
+       if (Session::has('session') and Session::get('session')['role']=="admin"){
+        $users= Users::with("get_role")->orderBy('created_at', 'desc')->get();
        
-        if (Users::where('username', $username)->exists()) {
-            $data= Users::where("username", $username)->get()[0];
-            // $check_pass= Users::where("password", Hash::make($password));
-            // print_r($check_pass);die();
-            
+    
+        // print_r($users);die();
+
+        return view("get_all_users", ["users"=>$users]);
+       }
+
+       else {
+        return abort(404);
+        // return redirect()->to("/login");
+       }
+       
+       public function delete_user(Request $request){
+  if($request->id){
+    $id= $request->id;
+    $query= Users::where('id', $id)->delete();
+     return $query;
+  }
+  else {
+      if (Session::has('session')){
+          $id= Session::get('session')["id"];
+           $query= Users::where('id', $id)->delete();
+           if ($query==1){
+               Session::forget("session");
+               return $query;
+           }
+      }
+      
+  }
+    //return redirect()->back();
+
+}
+
+
+  //controller__users
+public function forgot_password(Request $request){
+    global $email;
+    $email= $request->email;
+    
+
+     if (Users::where('email', $email)->exists()){
+         $token= bin2hex(random_bytes(16));
+         $user_id= Users::where("email", $email)->get()[0]->id;
+         Tokens::create(compact("user_id", "token"));
+        $data = array('link'=>"http://152.70.56.180:9001/reset_password/".$token);
+         Mail::send('mail', $data, function($message) use ($email) {
+         $message->to($email, 'My Articles')->subject
+            ('Reset your password');
+         $message->from('admin@myarticles.com','Yasar Unlu');
+      });
+      
+      return view("forgot_password",["response"=>"success"]);
+       
+     }
+     else {
+         return view("forgot_password",["response"=>"failed"]);
+     }
+     
+     public function  reset_password($token){
+    if (Tokens::where('token', $token)->exists()){
+        $user_id= Tokens::where("token", $token)->get()[0]->user_id;
+        Tokens::where("token", $token)->delete();
+        return view("reset_password", ["user_id"=>$user_id]);
+    } 
+}
+
+  //controller__users
+public function change_password (Request $request){
+    $password= $request->password;
+    $current_password= $request->current_password;
+    $request->validate([
+        "current_password"=>"required",
+        "password"=>"required|min:6|max:15|confirmed",
+        ]);
+    
+        if (Session::has('session')){
+            $user_id= Session::get('session')["id"];
  
-            if (md5($password)==$data->password){
-                $session_data= array(
-                    "username"=>$username,
-                    "name"=>$data->firstname." ".$data->lastname,
-                    "foto"=>$data->foto,
-                    "role"=>$data->role,
-                    "login_count"=>$data->login_count
-                );
-                Session::put("session", $session_data);
-
-
-               return redirect()->to("/");
-        
-            }  
-            else{
-                return redirect()->to("/login")->with(["message"=>"login is failed"]);  
-             }  
-         }
-        else{
-           return redirect()->to("/login")->with(["message"=>"login is failed"]);  
+            $user_password= Users::where("id",$user_id)->get()[0]->password;
+            if ($user_password== md5($current_password)){
+                $check= Users::where("id",$user_id)->update([
+                    "password"=>md5($password)
+                ]);
+                return view("change_pass", ["response"=>$check]);
+            }
+            else {
+                return view("change_pass", ["response"=>"Current password is not correct"]);
+            }
+            
         }
-       
-        // Session::forget("username");
 
-        
-    }
+}
+
+  //controller__users
+public function update_password(Request $request){
+    $id= $request->user_id;
+    
+    $request->validate([
+        "password"=>"required|min:6|max:15|confirmed",
+        ]);
+
+        Users::find($id)->update([
+            "password"=>md5($request->password)
+        ]);
+        return view("login", ["response"=>"success"]);
+
+}
+
+
+
+ 
+    
+}
+    
+}
+    
+    
+    
 }
