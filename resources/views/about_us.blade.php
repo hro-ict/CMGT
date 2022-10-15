4031f3dd582533b514b6a772f957417564f95ccb
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-10-15 22:57:55 +0200

    Create about_us.blade.php

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,38 @@
+@extends('master')
+@section('content')
+<div class="container mt-5 pt-5">
+            <h1>About Us</h1>
+            <h5>
+                        myarticles.com  is a platform where members publish their own articles.
+Everyone who is a member of our platform agrees to the following rules.
+            </h5>
+            
+
+ <ul>
+     
+ <li>
+  It is forbidden to write articles and comments that contain violence, insults and discrimination. Such posts will be deleted immediately.   
+ </li>    
+ <li>
+ The legal responsibility of the articles written by the members belongs to them.    
+ </li>
+ <li>
+  The copyright of the articles written belongs to myarticles.com.   
+ </li>
+ <li>
+     Articles are published after they are approved by the admin.
+ </li>
+ <li>
+     Admin can delete articles or comments
+ </li>
+ <li>
+     Admin can cancel the membership of the members
+ </li>
+ <li>
+     Members must leave at least 3 comments in order to write articles on the platform.
+ </li>
+ </ul>
+
+</div>
+@endsection
+
