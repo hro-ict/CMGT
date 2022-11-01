0dd047eae246ee8c9a8e6722b0736f5ddbc4fb1d
Yasar Unlu <57659697+hro-ict@users.noreply.github.com>
2022-11-01 01:24:53 +0100

    Update Users.php

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -1,12 +1,35 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Articles;
+use App\Models\Comments;
 
 class Users extends Model
 {
     use HasFactory;
-    protected $table= "users";
-    protected $fillable= ["firstname", "lastname","email","username","password","foto", "role_id", "created_at"];
+    protected $table="users";
+    protected $fillable= ["firstname", "lastname","email","username","password","foto", "role_id", "created_at","updated_at"];
+    
+    public function get_user_articles(){
+        return $this->hasMany(Articles::class, "user_id");
+        
+    }
+    
+     public function get_user_comments(){
+        return $this->hasMany(Comments::class, "user_id");
+        
+    }
+    
+    public function get_role(){
+        return $this->hasOne(Roles::class, "id", "role_id");
+    }
+
+    public function get_user_token(){
+        return $this->hasOne(Tokens::class, "id", "user_id");
+
+    }
 }
+
